b9cd30a0b654ee9fc340648b9fc2ff8e4b7f30ce
naithy <naithyDev@gmail.com>
2023-11-06 02:28:39 +0400

    dsds

--- routes/customers.js
+++ routes/customers.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 const { getCustomers, deleteCustomer } = require('../controllers/customers');
 
 const router = express.Router();
 
+
+const storage = multer.diskStorage({
+});
+
 router.get('/', getCustomers);
-router.delete('/:id', deleteCustomer)
+router.delete('/:id', upload.single(), deleteCustomer)
 
 
 module.exports = router;
