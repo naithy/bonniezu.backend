6bf58ec1ae1e79ccc3b69b4ee75107c5ecf5a3b9
naithy <naithyDev@gmail.com>
2023-11-21 13:53:54 +0400

    add inline keyboard

--- controllers/invoiceLink.js
+++ controllers/invoiceLink.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const Customer = require('../models/customer');
+const crypto = require('crypto');
 
 const TOKEN = process.env.TOKEN;
 const PROVIDER_TOKEN = process.env.PROVIDER_TOKEN;
@@ -9,43 +10,45 @@ const bot = new TelegramBot(TOKEN, {polling: true});
 
 const getInvoiceLink = async (req, res) => {
 
-    const errors = {};
+    // const errors = {};
 
-    if(!req.body.title) {
-        errors.title = {message: "Пожалуйста, укажите название"}
-    }
+    // if(!req.body.title) {
+    //     errors.title = {message: "Пожалуйста, укажите название"}
+    // }
 
-    if(!req.body.price) {
-        errors.price = {message: "Пожалуйста, укажите название"}
-    }
+    // if(!req.body.price) {
+    //     errors.price = {message: "Пожалуйста, укажите название"}
+    // }
 
-    if (Object.keys(errors).length > 0) {
-        return res.status(400).json(errors)
-    }
+    // if (Object.keys(errors).length > 0) {
+    //     return res.status(400).json(errors)
+    // }
     
 
-    try {
-        const { title, price, photoUrl, data } = req.body;
+    // try {
+    //     const { title, price, data } = req.body;
         
-        const invoiceLink = await bot.createInvoiceLink(
-            title, 
-            description='...', 
-            payload=data,
-            provider_token=PROVIDER_TOKEN, 
-            currency='RUB', 
-            prices=[
-                {
-                    label: title,
-                    amount: +price * 100
-                }
-            ],)
-        res.status(201).json(invoiceLink)
-    } catch (error) {
-        res.status(500).json({
-            message: "Не удалось создать ссылку для оплаты"
-        });
-    }
+    //     const invoiceLink = await bot.createInvoiceLink(
+    //         title, 
+    //         description='...', 
+    //         payload=data,
+    //         provider_token=PROVIDER_TOKEN, 
+    //         currency='RUB', 
+    //         prices=[
+    //             {
+    //                 label: title,
+    //                 amount: +price * 100
+    //             }
+    //         ],)
+    //     res.status(201).json(invoiceLink)
+    // } catch (error) {
+    //     res.status(500).json({
+    //         message: "Не удалось создать ссылку для оплаты"
+    //     });
+    // }
 }
+
+
 
 bot.on("pre_checkout_query", (msg) => { bot.answerPreCheckoutQuery(msg.id, true); });
 
@@ -68,6 +71,22 @@ bot.on("successful_payment", async (msg) => {
 
     bot.sendMessage(msg.chat.id, 'Товар был куплен, ожидайте связи с продавцом')
 }); 
+
+
+bot.on("message", async (msg) => {
+    const text = msg.text
+    bot.sendMessage(msg,chat,id, 'Переходи в интернет магазин по кнопке ниже!')
+
+    if(text === '/start') {
+        await bot.sendMessage(msg.chat.id, 'Переходи в магазин по кнопке ниже!', {
+            reply_markup: {
+                inline_keyboard: [{text: 'Магазин', web_app: {url: 'https://comfy-blini-ab6f36.netlify.app/'}}]
+            }
+        })
+    }
+
+})
+
 
 module.exports = {
     getInvoiceLink,
