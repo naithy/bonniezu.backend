0d86c3af685eb9aee75826428a77580f7edd0768
naithy <naithyDev@gmail.com>
2023-12-06 21:15:29 +0400

    ds

--- controllers/customers.js
+++ controllers/customers.js
@@ -20,11 +20,11 @@ const createCustomer = async (req, res) => {
             method, 
             custom, 
             underpayment 
-        } = req.body
+        } = await req.body
         const data = [SECRET, desc, currency, shop, payment_id, amount]
         const sign2 = crypto.createHash('md5').update(data.join('|')).digest('hex')
         
-        console.log('work', data)
+        console.log('work', sign2)
 
         if (sign2 === sign) {
             await Customer.create({
