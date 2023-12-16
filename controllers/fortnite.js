f8f0216d7c8124eaee77691d75098ef588e2bd17
naithy <naithyDev@gmail.com>
2023-12-16 21:50:04 +0400

    ds

--- controllers/fortnite.js
+++ controllers/fortnite.js
@@ -5,7 +5,7 @@ const getFortnite = async (req, res) => {
         
         const fortnite = await Fortnite.find();
 
-        res.status(200).json(Fortnite);
+        res.status(200).json(fortnite);
     } catch (error) {
         res.status(500).json({
             message: "Не удалось получить информацию о сервисе Fortnite"
